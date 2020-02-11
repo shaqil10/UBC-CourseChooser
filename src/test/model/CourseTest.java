2237a66c4e6d2c67310eeb995166c5496dcd3f6b
shaqil10 <58369573+shaqil10@users.noreply.github.com>
2020-02-11 13:48:31 -0800

    implemented all tests for getters in model package

--- src/test/model/CourseTest.java
+++ src/test/model/CourseTest.java
@@ -1,12 +1,33 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CourseTest {
+    Course c15;
+
+    @BeforeEach
+    public void runBefore() {
+        c15 = new Course("UBC-2017W-COMM-457-101", "2017W", "COMM", "457",
+                "101", "FNDTLS FIN ACCT", new Instructor("Sinclair, Scott", "4.5/5"),
+                121, new Stats(70.34, 14.74, 99, 32, 107, 11, 3),
+                new Grades(0, 0, 0, 2, 9, 11, 7,
+                        9, 13, 10, 10, 7, 12,
+                        15, 15, 9));
+    }
 
     @Test
-    public void testInfoToString() {
+    public void testGetters() {
+         assertEquals("UBC-2017W-COMM-457-101", c15.getId());
+         assertEquals("2017W",c15.getYearsession());
+         assertEquals("COMM",c15.getSubject());
+         assertEquals("457", c15.getCourseNum());
+         assertEquals("101", c15.getSection());
+         assertEquals("FNDTLS FIN ACCT", c15.getTitle());
+         assertEquals("Sinclair, Scott", c15.getInstructor().getName());
+         assertEquals("4.5/5", c15.getInstructor().getRating());
+         assertEquals(121,c15.getEnrolled());
     }
 }
--- src/test/model/StatsTest.java
+++ src/test/model/StatsTest.java
@@ -1,12 +1,35 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsTest {
+    Course c15;
+    Stats c15stats;
+
+
+    @BeforeEach
+    public void runBefore() {
+        c15 = new Course("UBC-2017W-COMM-457-101", "2017W", "COMM", "457",
+                "101", "FNDTLS FIN ACCT", new Instructor("Sinclair, Scott", "4.5/5"),
+                121, new Stats(70.34, 14.74, 99, 32, 107, 11, 3),
+                new Grades(0, 0, 0, 2, 9, 11, 7,
+                        9, 13, 10, 10, 7, 12,
+                        15, 15, 9));
+        c15stats = c15.getStats();
+    }
 
     @Test
-    public void testStatsToString() {
+    public void testGetters() {
+        assertEquals(70.34, c15stats.getAverage());
+        assertEquals(14.74, c15stats.getStDev());
+        assertEquals(99, c15stats.getHigh());
+        assertEquals(32, c15stats.getLow());
+        assertEquals(107, c15stats.getPass());
+        assertEquals(11,c15stats.getFail());
+        assertEquals(3, c15stats.getWithdrew());
+
     }
 }
